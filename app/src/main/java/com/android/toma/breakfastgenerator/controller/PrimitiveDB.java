598023b4e06cb4cb67077b70ee3f4e7f462ea8c6
toma3490 <toma3490@gmail.com>
2017-01-28 12:15:58 +0200

    Add pictures

--- app/src/main/java/com/android/toma/breakfastgenerator/controller/PrimitiveDB.java
+++ app/src/main/java/com/android/toma/breakfastgenerator/controller/PrimitiveDB.java
@@ -42,13 +42,13 @@ public class PrimitiveDB {
         ingrDish6.addIngr("Jam");
 
         // Dishes
-        Dish dish0 = new Dish("Pancakes with jam", 30, ingrDish0, R.drawable.test);
-        Dish dish1 = new Dish("Pancakes with banana and nutella", 30, ingrDish1, R.drawable.test2);
-        Dish dish2 = new Dish("Muesli with nuts", 15, ingrDish2, 1);
-        Dish dish3 = new Dish("Omelet with vegetables", 25, ingrDish3, 1);
-        Dish dish4 = new Dish("Egg-bread with tomato and cheese", 20, ingrDish4, 1);
-        Dish dish5 = new Dish("Cottage cheese with jam", 5, ingrDish5, 1);
-        Dish dish6 = new Dish("Yogurt with jam", 5, ingrDish6, 1);
+        Dish dish0 = new Dish("Pancakes with jam", 30, ingrDish0, R.drawable.cat1d0);
+        Dish dish1 = new Dish("Pancakes with banana and nutella", 30, ingrDish1, R.drawable.cat1d1);
+        Dish dish2 = new Dish("Muesli with nuts", 15, ingrDish2, R.drawable.cat1d2);
+        Dish dish3 = new Dish("Omelet with vegetables", 25, ingrDish3, R.drawable.cat1d3);
+        Dish dish4 = new Dish("Egg-bread with tomato and cheese", 20, ingrDish4, R.drawable.cat1d4);
+        Dish dish5 = new Dish("Cottage cheese with jam", 5, ingrDish5, R.drawable.cat1d5);
+        Dish dish6 = new Dish("Yogurt with jam", 5, ingrDish6, R.drawable.cat1d6);
 
         // Add dishes to list
         firstCategoryDishes.add(dish0);
@@ -87,14 +87,14 @@ public class PrimitiveDB {
         ingrDish6.addIngr("Peanut butter");
 
         // Dishes
-        Dish dish0 = new Dish("Boiled egg", 10, ingrDish0, 1);
-        Dish dish1 = new Dish("Fried egg", 10, ingrDish0, 1);
-        Dish dish2 = new Dish("Sandwich with butter", 5, ingrDish1, 1);
-        Dish dish3 = new Dish("Sandwich with processed cheese and tomato", 5, ingrDish2, 1);
-        Dish dish4 = new Dish("Sandwich with tuna", 5, ingrDish3, 1);
-        Dish dish5 = new Dish("Egg toast with cheese", 10, ingrDish4, 1);
-        Dish dish6 = new Dish("Toast with processed cheese", 10, ingrDish5, 1);
-        Dish dish7 = new Dish("Sandwich with peanut butter", 5, ingrDish6, 1);
+        Dish dish0 = new Dish("Boiled egg", 10, ingrDish0, R.drawable.cat2d0);
+        Dish dish1 = new Dish("Fried egg", 10, ingrDish0, R.drawable.cat2d1);
+        Dish dish2 = new Dish("Sandwich with butter", 5, ingrDish1, R.drawable.cat2d2);
+        Dish dish3 = new Dish("Sandwich with processed cheese and tomato", 5, ingrDish2, R.drawable.cat2d3);
+        Dish dish4 = new Dish("Sandwich with tuna", 5, ingrDish3, R.drawable.cat2d4);
+        Dish dish5 = new Dish("Egg toast with cheese", 10, ingrDish4, R.drawable.cat2d5);
+        Dish dish6 = new Dish("Toast with processed cheese", 10, ingrDish5, R.drawable.cat2d6);
+        Dish dish7 = new Dish("Sandwich with peanut butter", 5, ingrDish6, R.drawable.cat2d7);
 
         ArrayList<Food> firstDish = new ArrayList<>();
         firstDish.add(dish0);
@@ -136,12 +136,12 @@ public class PrimitiveDB {
         ingrDrink5.addIngr("Milk");
 
         // Drinks
-        Drink drink0 = new Drink("Black tea with lemon", 10, ingrDrink0, 1);
-        Drink drink1 = new Drink("Green tea with lemon", 10, ingrDrink1, 1);
-        Drink drink2 = new Drink("Coffee with milk", 10, ingrDrink2, 1);
-        Drink drink3 = new Drink("Cocoa with milk", 10, ingrDrink3, 1);
-        Drink drink4 = new Drink("Fruit juice", 2, ingrDrink4, 1);
-        Drink drink5 = new Drink("Hot or cold milk", 2, ingrDrink5, 1);
+        Drink drink0 = new Drink("Black tea with lemon", 10, ingrDrink0, R.drawable.drink0);
+        Drink drink1 = new Drink("Green tea with lemon", 10, ingrDrink1, R.drawable.drink1);
+        Drink drink2 = new Drink("Coffee with milk", 10, ingrDrink2, R.drawable.drink2);
+        Drink drink3 = new Drink("Cocoa with milk", 10, ingrDrink3, R.drawable.drink3);
+        Drink drink4 = new Drink("Fruit juice", 2, ingrDrink4, R.drawable.drink4);
+        Drink drink5 = new Drink("Hot or cold milk", 2, ingrDrink5, R.drawable.drink5);
 
         // Add dishes to list
         drinks.add(drink0);
@@ -157,10 +157,6 @@ public class PrimitiveDB {
 
     public ArrayList<Food> getFirstCategoryDishes(){
         return firstCategoryDishes;
-    }
-
-    public ArrayList<ArrayList<Food>> getSecondCategoryDishes(){
-        return secondCategoryDishes;
     }
 
     public ArrayList<Food> getSecondCategoryDishesFirstDish(){
